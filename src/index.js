b3ec8fd9856c24e611544687d6b7478f3c3553a0
spikeburton <ralphburton3@gmail.com>
2019-03-21 11:05:48 -0400

    add functionality to render quote

--- src/index.js
+++ src/index.js
@@ -7,6 +7,60 @@ document.addEventListener("DOMContentLoaded", () => {
   renderAllQuotes();
 })
 
-function renderAllQuotes() {
+function getAllQuotes() {
+  return fetch(QUOTES_URL).then(response => response.json())
+}
 
+function renderAllQuotes() {
+  document.getElementById('quote-list').innerHTML = '';
+
+  getAllQuotes()
+  .then(quotes => quotes.forEach(renderQuote))
+}
+
+function renderQuote(data) {
+  console.log(data)
+  const container = document.getElementById('quote-list');
+
+  const item = document.createElement('li');
+  item.className = 'quote-card';
+  container.appendChild(item);
+
+  const blockquote = document.createElement('blockquote');
+  blockquote.className = 'blockquote'
+  item.appendChild(blockquote);
+
+  const p = document.createElement('p');
+  p.className = 'mb-0';
+  p.innerText = data.quote;
+  blockquote.appendChild(p);
+
+  const footer = document.createElement('footer');
+  footer.className = 'blockquote-footer';
+  footer.innerText = data.author;
+  blockquote.appendChild(footer);
+
+  blockquote.appendChild(document.createElement('br'))
+
+  const likesButton = document.createElement('button');
+  likesButton.className = 'btn-success';
+  likesButton.dataset.id = data.id;
+  likesButton.innerHTML = `Likes: <span>${data.likes}</span>`;
+  likesButton.addEventListener('click', handleLikes)
+  blockquote.appendChild(likesButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'btn-danger';
+  deleteButton.dataset.id = data.id;
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener('click', handleDelete)
+  blockquote.appendChild(deleteButton);
+}
+
+function handleLikes(e) {
+  console.log(e.target)
+}
+
+function handleDelete(e) {
+  console.log(e.target);
 }
