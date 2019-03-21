36a783c318c252ade33c62e3c055b43cf874fa76
spikeburton <ralphburton3@gmail.com>
2019-03-21 10:43:15 -0400

    add dom content loaded event listener

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// It might be a good idea to add event listener to make sure this file 
+// It might be a good idea to add event listener to make sure this file
 // only runs after the DOM has finshed loading.
+const QUOTES_URL = "http://localhost:3000/quotes"
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log("load successful")
+  renderAllQuotes();
+})
+
+function renderAllQuotes() {
+
+}
